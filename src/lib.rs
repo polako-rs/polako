6338c95298d1be0fa23804ec31325dbfbf162bb5
Yakov Borevich <jkbo@funexpected.org>
2023-09-22 00:47:20 +0200

    drop unused deps

--- src/lib.rs
+++ src/lib.rs
@@ -1,11 +1,12 @@
 #![doc(html_logo_url = "https://github.com/jkb0o/polako/raw/master/docs/polakoko.png")]
 
 pub mod constructivism {
+    pub use polako_constructivism::*;
+    pub use polako_macro::*;
     pub mod prelude {
         pub use polako_constructivism::{design, Construct, ConstructItem, Segment};
         pub use polako_macro::*;
     }
-    pub use polako_constructivism::*;
 }
 
 pub mod eml {
